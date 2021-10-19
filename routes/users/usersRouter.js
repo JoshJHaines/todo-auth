360fbb66978daa81094cd829f858378de07796b2
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-19 16:35:39 -0400

    added comment boxes for further organization and notes

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var router = express.Router();
-
+/************************************
+ * IMPORT FUNCTIONS FROM CONTROLLER *
+ ************************************/
 const { createUser, deleteUserById } = require("./controller/userController");
+/******************************
+ * CRUD ROUTES WITH FUNCTIONS *
+ ******************************/
 
 /*******************
  * MAIN USERS PAGE *
@@ -27,6 +32,9 @@ router.post(
 /*********************
  * DELETE USERS PAGE *
  *********************/
- router.delete("/delete-user-by-id/:id", deleteUserById);
+router.delete("/delete-user-by-id/:id", deleteUserById);
 
+/**************************************
+ * EXPORT ROUTER TO BE USED ELSEWHERE *
+ **************************************/
 module.exports = router;
