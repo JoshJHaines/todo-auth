872636a840f5a1c8eb3cea0868f425226c116c38
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-21 11:49:01 -0400

    storing decodedToken in local

--- routes/users/lib/shared/jwtMiddleware.js
+++ routes/users/lib/shared/jwtMiddleware.js
@@ -1,19 +1,22 @@
 const jwt = require("jsonwebtoken");
 
-async function jwtMiddleware(req, res, next) {
+function jwtMiddleware(req, res, next) {
 	try {
 		if (req.headers && req.headers.authorization) {
 			let notDecodedToken = req.headers.authorization;
+
 			let slicedToken = notDecodedToken.slice(7);
+
 			let decodedToken = jwt.verify(slicedToken, process.env.JWT_SECRET);
 
-			//* need to find the token in headers and slice off the 'Bearer '
-			res.json({ message: "SUCCESSSSS", token: decodedToken });
+			res.locals.decodedData = decodedToken;
+
+			next();
 		} else {
-			throw { message: "You do not have permission. None Shall Pass!!!" };
+			throw { message: "You don't have permission" };
 		}
-	} catch (error) {
-		res.status(500).json({ message: "error", error: error.message });
+	} catch (e) {
+		res.status(500).json({ message: "error", error: e.message });
 	}
 }
 
