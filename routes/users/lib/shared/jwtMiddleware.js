454aea39aba52f04d73a3eeb434ebedc2d5ee0d2
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-21 10:42:17 -0400

    added if statement to try block

--- routes/users/lib/shared/jwtMiddleware.js
+++ routes/users/lib/shared/jwtMiddleware.js
@@ -2,10 +2,16 @@ const jwt = require("jsonwebtoken");
 
 async function jwtMiddleware(req, res, next) {
 	try {
-        console.log(req.headers.authorization);
-        //* need to find the token in headers and slice off the 'Bearer '
-		let decodedToken = jwt.verify(req.headers.authorization.slice(7), process.env.JWT_SECRET);
-		res.json({ message: "SUCCESSSSS", token: decodedToken });
+		if (req.headers && req.headers.authorization) {
+			let notDecodedToken = req.headers.authorization;
+			let slicedToken = notDecodedToken.slice(7);
+			let decodedToken = jwt.verify(slicedToken, process.env.JWT_SECRET);
+
+			//* need to find the token in headers and slice off the 'Bearer '
+			res.json({ message: "SUCCESSSSS", token: decodedToken });
+		} else {
+			throw { message: "You do not have permission. None Shall Pass!!!" };
+		}
 	} catch (error) {
 		res.status(500).json({ message: "error", error: error.message });
 	}
