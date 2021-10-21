c1cb7849a302996483a295108a820119a54e3312
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-21 10:16:53 -0400

    imported jwtMiddleware into central index

--- routes/users/lib/index.js
+++ routes/users/lib/index.js
@@ -1,12 +1,14 @@
 const { validateCreateUser } = require("./authCreateMiddleware/validateCreateUser");
 const { validateLoginData } = require("./authLoginMiddleware/validateLoginData")
 const { checkIsUndefined } = require("./shared/checkIsUndefined")
-const { checkIsEmpty} = require("./shared/checkIsEmpty");
+const { checkIsEmpty } = require("./shared/checkIsEmpty");
+const { jwtMiddleware } = require("./shared/jwtMiddleware")
 
 
 module.exports = {
 	validateCreateUser,
 	validateLoginData,
 	checkIsUndefined,
-	checkIsEmpty
+	checkIsEmpty,
+	jwtMiddleware
 };
