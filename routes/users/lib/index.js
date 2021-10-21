ea29e00063188324605ec803403fa534ac86dae5
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-21 08:21:50 -0400

    added checkIsEmpty middleware

--- routes/users/lib/index.js
+++ routes/users/lib/index.js
@@ -1,7 +1,9 @@
 const { validateCreateUser } = require("./authCreateMiddleware/validateCreateUser");
 const { checkIsUndefined } = require("./authLoginMiddleware/checkIsUndefined")
+const { checkIsEmpty} = require("./authLoginMiddleware/checkIsEmpty")
 
 module.exports = {
 	validateCreateUser,
-	checkIsUndefined
+	checkIsUndefined,
+	checkIsEmpty
 };
